235a9e56414fb26155e34e94a6ef7eee1c649bc2
Pär Karlsson <pariz@MacBook-Pro-som-tillhor-Par.local>
2024-04-03 09:53:30 +0200

    Add fallback logic when resolving configuration

--- lib/configuration/environment_configuration_config.dart
+++ lib/configuration/environment_configuration_config.dart
@@ -1,32 +1,23 @@
 class EnvironmentConfigurationConfig {
-  static const String ovhEUProduction =
-      'https://storage.gra.cloud.ovh.net/v1/AUTH_fc1ba5ae6eb64e10a8c0b2499d0e8ca9/production/configs/';
-  static const String googleEUProduction =
-      'https://ebbot-v2.storage.googleapis.com/configs/';
-  static const String googleCanadaProduction =
-      'https://ebbot-ca.storage.googleapis.com/configs/';
-
-  static const String release =
-      'https://ebbot-release.storage.googleapis.com/configs/';
-  static const String staging =
-      'https://ebbot-staging.storage.googleapis.com/configs/';
+  static const Map<Environment, String> baseUrls = {
+    Environment.ovhEUProduction:
+        'https://storage.gra.cloud.ovh.net/v1/AUTH_fc1ba5ae6eb64e10a8c0b2499d0e8ca9/production/configs/',
+    Environment.googleEUProduction:
+        'https://ebbot-v2.storage.googleapis.com/configs/',
+    Environment.googleCanadaProduction:
+        'https://ebbot-ca.storage.googleapis.com/configs/',
+    Environment.release:
+        'https://ebbot-release.storage.googleapis.com/configs/',
+    Environment.staging: 'https://ebbot-staging.storage.googleapis.com/configs/'
+  };
 
   // Function to get the appropriate URL based on environment
   static String getConfigUrl(Environment environment) {
-    switch (environment) {
-      case Environment.ovhEUProduction:
-        return ovhEUProduction;
-      case Environment.googleEUProduction:
-        return googleEUProduction;
-      case Environment.googleCanadaProduction:
-        return googleCanadaProduction;
-      case Environment.release:
-        return release;
-      case Environment.staging:
-        return staging;
-      default:
-        throw Exception("Unknown environment");
+    final baseUrl = baseUrls[environment];
+    if (baseUrl != null) {
+      return baseUrl;
     }
+    throw Exception("Unknown environment");
   }
 }
 
--- lib/src/network/ebbot_http_client.dart
+++ lib/src/network/ebbot_http_client.dart
@@ -26,15 +26,42 @@ class EbbotHttpClient {
         "$configBaseUrl$botId.json?t=${DateTime.now().millisecondsSinceEpoch}");
     logger.i("Fetching config from environment $env and uri $uri");
 
+    // First try to fetch the config for the given environment
+    var config = await _fetchConfig(env);
+    if (config != null) {
+      return config;
+    }
+    
+    // Then try to fetch the config for all the other environments
+    for (var host in EnvironmentConfigurationConfig.baseUrls.keys) {
+      var config = await _fetchConfig(host);
+      if (config != null) {
+        return config;
+      }
+    }
+
+    throw Exception("Failed to fetch config for env $env at uri $uri");
+  }
+
+  Future<ChatConfig?> _fetchConfig(Environment env) async {
+    var configBaseUrl = EnvironmentConfigurationConfig.getConfigUrl(env);
+    final uri = Uri.parse(
+        "$configBaseUrl$botId.json?t=${DateTime.now().millisecondsSinceEpoch}");
     try {
       final response =
           await http.get(uri, headers: {'Accept-Charset': 'utf-8'});
+
+      if (response.statusCode < 200 || response.statusCode >= 300) {
+        logger.w("Failed to fetch config for env $env at uri $uri");
+        return null;
+      }
+
       final decodedBody = utf8.decode(response.bodyBytes);
       final jsonResponse = json.decode(decodedBody);
       return ChatConfig.fromJson(jsonResponse);
     } catch (e) {
       logger.e("Error fetching config for env $env at uri $uri: $e");
-      rethrow;
+      return null;
     }
   }
 }
